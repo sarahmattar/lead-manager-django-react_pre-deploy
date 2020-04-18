879b922a64a53c13378a0fda8ae9c9b66e3c8789
Sarah <sarah.mattar@mac.com>
2020-04-17 17:30:25 -0700

    restructure for subtree deploy

--- lead_manager/lead_manager/settings.py
+++ lead_manager/lead_manager/settings.py
@@ -20,7 +20,7 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
-environ.Env.read_env('../.env')
+environ.Env.read_env('./.env')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
